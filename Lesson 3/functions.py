990364c31378f1701a66b0fc3be60873580cbf39
boggomo <bogdan.gomoescu@gmail.com>
2020-02-20 10:33:14 +0200

    test_vscode_git

---
+++ Lesson 3/functions.py
@@ -0,0 +1,15 @@
+import regex
+
+
+def hello():
+    print("Howdy!")
+    print("How 5are you?")
+    print("hello there")
+
+
+def plusOne(number):
+    return number + 1
+
+
+newNumber = input("Enter the lucky number: ")
+print("Hello, ", newNumber)
